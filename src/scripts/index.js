24923d3740d7dd365b6904b6146e863f0456f812
godested <godested@gmail.com>
2018-03-02 23:16:48 +0200

    Refactor styles and add hamburger toggle

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,4 +3,10 @@ import { TimelineLite, Power1 } from 'gsap';
 
 $(document).ready(function () {
   const timeLine = new TimelineLite();
+
+  const menuButton = $('.menu-button');
+
+  $(menuButton).click(function () {
+    $(menuButton).toggleClass('is-active');
+  });
 });
