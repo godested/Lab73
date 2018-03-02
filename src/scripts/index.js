cc363dff0f1973dbb21ad6c39e7a5dc23c412a29
godested <godested@gmail.com>
2018-03-02 22:02:27 +0200

    Add simple layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,8 +3,4 @@ import { TimelineLite, Power1 } from 'gsap';
 
 $(document).ready(function () {
   const timeLine = new TimelineLite();
-
-  const logo = $('.logo');
-
- // timeLine.fromTo(logo, 1, {opacity: 0, y: -100}, {opacity: 1, y: 0, ease: Power1.easeIn})
 });
