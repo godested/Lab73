08edfe3a11d2d7344f0f00a6929dce71ebc38756
godested <godested@gmail.com>
2018-03-02 18:59:29 +0200

    Add hero

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,9 +2,9 @@ import $ from 'jquery';
 import { TimelineLite, Power1 } from 'gsap';
 
 $(document).ready(function () {
-  const tl = new TimelineLite();
+  const timeLine = new TimelineLite();
 
   const logo = $('.logo');
 
- // tl.fromTo(logo, 1, {opacity: 0, y: -100}, {opacity: 1, y: 0, ease: Power1.easeIn})
+ // timeLine.fromTo(logo, 1, {opacity: 0, y: -100}, {opacity: 1, y: 0, ease: Power1.easeIn})
 });
