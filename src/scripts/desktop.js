4a3ee8247d7ba5540c5392491f13c905c6074605
godested <godested@gmail.com>
2018-03-03 02:13:32 +0200

    Improve nodes names

--- src/scripts/desktop.js
+++ src/scripts/desktop.js
@@ -5,28 +5,33 @@ import $ from "jquery";
 const desktop = function () {
   const timeLine = new TimelineMax();
 
-  const wrapper = $('.wrapper');
   const header = $('.header');
-  const parallaxLayers = $('.parallax__layer');
-  const title = $('.hero__title');
-  const description = $('.hero__description');
-  const keywordBorder = $('.hero__keyword .border');
-  const pageNavigation = $('.page-navigation__link');
+  const hero = $('.hero');
+  const heroTitle = $('.hero__title');
+  const heroDescription = $('.hero__description');
+  const heroKeywordBorder = $('.hero__keyword .border');
+  const pageNavigation = $('.page-navigation');
+  const pageNavigationLink = $('.page-navigation__link');
   let parallax;
+  const parallaxLayers = $('.parallax__layer');
+
+  hero.css('min-height', function () {
+    return window.innerHeight - header.outerHeight() - pageNavigation.outerHeight();
+  });
 
   timeLine.to(header, 0.5, {y: 0, ease: Power1.easeIn})
     .add('headerTransitionEnd')
-    .fromTo(title, 0.5, {y: 50}, {opacity: 1, y: 0}, 'headerTransitionEnd')
+    .fromTo(heroTitle, 0.5, {y: 50}, {opacity: 1, y: 0}, 'headerTransitionEnd')
     .add('titleTransitionEnd')
-    .fromTo(keywordBorder, 0.5, {width: '0'}, {width: '98%'}, 'titleTransitionEnd')
-    .fromTo(description, 0.5, {y: 50}, {opacity: 1, y: 0}, 'titleTransitionEnd')
+    .fromTo(heroKeywordBorder, 0.5, {width: '0'}, {width: '98%'}, 'titleTransitionEnd')
+    .fromTo(heroDescription, 0.5, {y: 50}, {opacity: 1, y: 0}, 'titleTransitionEnd')
     .staggerFromTo(parallaxLayers, 0.5, {scale: 0}, {scale: 1, opacity: 1}, 0.3, 'headerTransitionEnd')
     .add(function () {
       parallax = new Parallax(document.querySelector('.parallax'), {
         relativeInput: true
       });
     })
-    .staggerFromTo(pageNavigation, 0.5, {y: 50}, {opacity: 1, y: 0}, 0.5);
+    .staggerFromTo(pageNavigationLink, 0.5, {y: 50}, {opacity: 1, y: 0}, 0.5);
 
 
   const menuButton = $('.menu-button');
