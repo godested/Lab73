2f44e03ed53f0dc94187e3c08917da2a010984e5
godested <godested@gmail.com>
2018-03-05 22:37:13 +0200

    Change load animation

--- src/scripts/desktop/onLoadAnimation.js
+++ src/scripts/desktop/onLoadAnimation.js
@@ -5,8 +5,7 @@ import $ from 'jquery';
 const onLoadAnimation = function () {
   const timeLine = new TimelineMax();
 
-  const logo = $('.logo');
-  const menuButton = $('.menu-button');
+  const header = $('.header');
   const heroTitle = $('.hero__title');
   const heroDescription = $('.hero__description');
   const heroKeywordBorder = $('.hero__keyword .border');
@@ -14,19 +13,20 @@ const onLoadAnimation = function () {
   let parallax;
   const parallaxLayers = $('.parallax__layer');
 
-  timeLine.staggerTo([logo, menuButton], 0.5, {y: 0, ease: Power1.easeIn}, 0.5)
+  timeLine
+    .fromTo(header, 0.5, {y: -100}, {y: 0, ease: Power1.easeIn}, 0.5)
     .add('headerTransitionEnd')
-    .fromTo(heroTitle, 0.5, {y: 50}, {opacity: 1, y: 0}, 'headerTransitionEnd')
+    .fromTo(heroTitle, 0.5, {opacity: 0, y: 50}, {opacity: 1, y: 0}, 'headerTransitionEnd')
     .add('titleTransitionEnd')
-    .fromTo(heroKeywordBorder, 0.5, {width: '0'}, {width: '98%'}, 'titleTransitionEnd')
-    .fromTo(heroDescription, 0.5, {y: 50}, {opacity: 1, y: 0}, 'titleTransitionEnd')
-    .staggerFromTo(parallaxLayers, 0.3, {scale: 0.5}, {scale: 1, opacity: 1}, 0.5, 'headerTransitionEnd')
+    .fromTo(heroKeywordBorder, 0.5, {width: 0}, {width: '98%'}, 'titleTransitionEnd')
+    .fromTo(heroDescription, 0.5, {opacity: 0, y: 50}, {opacity: 1, y: 0}, 'titleTransitionEnd')
+    .staggerFromTo(parallaxLayers, 0.3, {opacity: 0, scale: 0.5}, {scale: 1, opacity: 1}, 0.5, 'headerTransitionEnd')
     .call(function () {
       parallax = new Parallax(document.querySelector('.parallax'), {
         relativeInput: true
       });
     })
-    .staggerFromTo(pageNavigationLink, 0.5, {y: 80}, {opacity: 1, y: 0}, 0.5);
+    .staggerFromTo(pageNavigationLink, 0.5, {opacity: 0, y: 80}, {opacity: 1, y: 0}, 0.5);
 };
 
 export default onLoadAnimation;
